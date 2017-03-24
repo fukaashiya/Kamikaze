05b5a23b56d794017d6500f01853848393790fe2
Fuka <hikaru.minagoshi@gmail.com>
2017-03-24 17:12:33 +0000

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kamikaze::Application.config.secret_key_base = '361e2341a0d6533843b48d8d064b577af1fc12325d6daf681ef12eec6c39314ab61113ca74462a0a5cc589973ad01575bb55629075f488e34e24047bcb98a0aa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kamikaze::Application.config.session_store :cookie_store, key: '_Kamikaze_session'
